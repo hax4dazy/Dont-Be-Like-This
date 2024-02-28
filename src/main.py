9071dce5f0ea48f7512fac569ae5e0b631e4a5e9
hax4dazy <Uitvinder.gdm@gmail.com>
2024-02-28 01:47:41 +0100

    Minor fixes

--- src/main.py
+++ src/main.py
@@ -9,7 +9,8 @@ try:
     discord_token = BotSettings.get(BotSettings.id == 1).BotToken
 except Exception as e:
     print(f'Token not found, taking env variable BOT_TOKEN and using that as the token.\nPlease remove the env variable and restart the bot')
-    BotSettings.create(BotToken=environ.get("BOT_TOKEN"))
+    with db.atomic() as txn:    
+        BotSettings.create(BotToken=environ.get("BOT_TOKEN"))
     exit()
 
 class YesOrNoButtons(discord.ui.View):
@@ -24,7 +25,9 @@ class YesOrNoButtons(discord.ui.View):
     
     @discord.ui.button(label="Yes", row=0, style=discord.ButtonStyle.danger)
     async def first_button_callback(self, button, interaction):
-        GuildSettings.update(ChannelID=self.channel_ID).where(GuildSettings.ServerID == interaction.message.guild.id).execute()
+        with db.atomic() as txn:
+            GuildSettings.update(ChannelID=self.channel_ID).where(GuildSettings.ServerID == interaction.message.guild.id).execute()
+        self.disable_all_items()
         await interaction.response.edit_message(content="Alright, the channel has been updated", view=self)
 
     @discord.ui.button(label="No", row=0, style=discord.ButtonStyle.primary)
@@ -38,24 +41,33 @@ async def on_ready():
 
 @bot.event
 async def on_guild_remove(guild: discord.Guild):
-    GuildSettings.delete().where(GuildSettings.ServerID == guild.id).execute()
+    with db.atomic() as txn:
+        GuildSettings.delete().where(GuildSettings.ServerID == guild.id).execute()
 
-@bot.slash_command(name="Setup", description="Set the channel where the bot will send the messages")
+@bot.slash_command(name="setup", description="Set the channel where the bot will send the messages")
 @discord.default_permissions(manage_messages=True)
 async def global_command(ctx: discord.ApplicationContext, channel: discord.TextChannel): 
         if GuildSettings.get_or_none(ServerID=ctx.guild.id) is None:
             webhook = await channel.create_webhook(name="Don't Be Like This Bot")
-            GuildSettings.create(ServerID=ctx.guild.id, ChannelID=channel.id, WebHookURL=webhook.url)
+            with db.atomic() as txn:    
+                GuildSettings.create(ServerID=ctx.guild.id, ChannelID=channel.id, WebHookURL=webhook.url)
             await ctx.respond(f"Set the channel to <#{channel.id}>!", ephemeral=True)
         else:
             await ctx.respond(f"It looks like you already have a channel set for messages.\nWould you like to update it?", view=YesOrNoButtons(timeout=20, channel_ID=channel.id), ephemeral=True) 
 
 @bot.message_command(name="Don't be like this")  
 async def dont_be_like_this(ctx: discord.ApplicationContext, message: discord.Message):  
-    try:
-        webhook_url = GuildSettings.get(GuildSettings.ServerID == ctx.guild.id).WebHookURL
-    except Exception as e:
-        await ctx.send_response(f"You cannot use the bot without setting it up first! Run the /setup command to set the bot up!")
+    if ctx.guild is None:
+        await ctx.send_response(f"You cannot use this command in DMs!", ephemeral=True)
+        return
+    if ctx.author.id == bot.user.id:
+        await ctx.send_response(f"I can't clown on my own messages!", ephemeral=True)
+        return
+    webhook_url = GuildSettings.get_or_none(GuildSettings.ServerID == ctx.guild.id)
+    if webhook_url is None:
+        await ctx.send_response(f"You cannot use the bot without setting it up first! Run the /setup command to set the bot up!", ephemeral=True)
+        return
+    webhook_url = webhook_url.WebHookURL
 
     async with aiohttp.ClientSession() as session:
         webhook = discord.Webhook.from_url(webhook_url, session=session)
