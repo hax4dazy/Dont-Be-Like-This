9db98057ef4970567d17eaf2e866e01c73c88bab
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2024-03-02 17:02:03 -0800

    send attachments along with webhook msg

--- src/main.py
+++ src/main.py
@@ -79,9 +79,12 @@ async def dont_be_like_this(ctx: discord.ApplicationContext, message: discord.Me
 
     # Defer the response to prevent the bot from timing out if the webhook takes too long to send the message (defer ephemeral)
     await ctx.response.defer(ephemeral=True)
+
+    message_attachments = [await attachment.to_file() for attachment in message.attachments]
     async with aiohttp.ClientSession() as session:
         webhook = discord.Webhook.from_url(webhook_url, session=session)
-        await webhook.send(content=f'[[Original Message](<{message.jump_url}>)] {message.content}', username=f'🤡{message.author.name}', avatar_url=message.author.avatar.url, allowed_mentions=discord.AllowedMentions.none())
-    await ctx.send_response(f"🤡 <@{message.author.id}>", ephemeral=True)
+        await webhook.send(content=f'[[Original Message](<{message.jump_url}>)] {message.content}', username=f'🤡 {message.author.name}', avatar_url=message.author.avatar.url, allowed_mentions=discord.AllowedMentions.none(), files=message_attachments)
+
+    await ctx.respond(f"🤡 Successfully clowned <@{message.author.id}>")
 
 bot.run(discord_token)
